68e92874be0686794dc01289952402e25f4b559c
Maycowpq225 <maycowfellipo@hotmail.com>
2025-05-13 16:30:12 -0300

    Retoques final

--- cypress/e2e/frontend/steps/checkout/CheckoutPA.js
+++ cypress/e2e/frontend/steps/checkout/CheckoutPA.js
@@ -1,28 +1,28 @@
 import CheckoutPO from "./CheckoutPO";
 
-class CheckoutPA extends CheckoutPO{
+class CheckoutPA extends CheckoutPO {
 
     preencherCamposObrigatorios() {
-        this.elementos.fieldNome().type('Teste');
-        this.elementos.fieldSobrenome().type('Santos');
-        this.elementos.fieldEndereço().type('Rua dos testes');
-        this.elementos.fieldNumero().type('20');
-        this.elementos.fieldCEP().type('08727000');
-        this.elementos.fieldTelefone().type('9978349864');
-        this.elementos.fieldEmail().type('Teste@teste.com');
+        this.elementos.fieldNome().type('Teste', { force: true });
+        this.elementos.fieldSobrenome().type('Santos', { force: true });
+        this.elementos.fieldEndereço().type('Rua dos testes', { force: true });
+        this.elementos.fieldNumero().type('20', { force: true });
+        this.elementos.fieldCEP().type('08727000', { force: true });
+        this.elementos.fieldTelefone().type('9978349864', { force: true });
+        this.elementos.fieldEmail().type('Teste@teste.com', { force: true });
 
     }
 
     escolherPagamento(metodoPagamento) {
-        this.elementos.listRadiosformPagamento().contains(metodoPagamento).siblings('input').click()
+        this.elementos.listRadiosformPagamento().contains(metodoPagamento).siblings('input').click({ force: true })
     }
-    
+
     aceitarTermosECondicoes() {
-        this.elementos.checkBoxAceiteTermos().click()
+        this.elementos.checkBoxAceiteTermos().click({ force: true })
     }
 
     finalizarPedido() {
-        this.elementos.btnFinalizarPedido().click()
+        this.elementos.btnFinalizarPedido().click({ force: true })
     }
 
     validarStatusDoPedido() {
@@ -33,8 +33,8 @@ class CheckoutPA extends CheckoutPO{
     validarErroCheckboxTermos() {
         this.elementos.checkBoxAceiteTermos()
             .siblings('.invalid-feedback')
-                .contains('Este campo é obrigatório.')
-                    .should('be.visible')
+            .contains('Este campo é obrigatório.')
+            .should('be.visible')
     }
 }
 export default CheckoutPA
